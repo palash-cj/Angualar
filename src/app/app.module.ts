0bc1443cc4e53de04a786ba71aa625330d76a021
Palash Jamaiwar <palashjemi@gmail.com>
2022-08-09 22:30:44 +0530

    First commit for angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyContainerComponent } from './mycontainer/mycontainer.component';
+import { TopnavComponent } from './mycontainer/topnav/topnav.component';
+import { HeaderComponent } from './mycontainer/header/header.component';
+import { Txtsec1Component } from './mycontainer/txtsec1/txtsec1.component';
+import { Txtsec2Component } from './mycontainer/txtsec2/txtsec2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyContainerComponent,
+    TopnavComponent,
+    HeaderComponent,
+    Txtsec1Component,
+    Txtsec2Component
   ],
   imports: [
     BrowserModule,
